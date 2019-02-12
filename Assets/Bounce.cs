e204d6b42829a8ab381ce5ed6e629116f4acebfe
StudentPegi <47355193+StudentPegi@users.noreply.github.com>
2019-02-12 16:11:36 +0100

    Bounce
    Bounce script, makes the ball jump
    -Pekka

---
+++ Assets/Bounce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.transform.GetComponent<Rigidbody>().AddForce(Vector3.up * 500f);
+        }
+    }
+}
